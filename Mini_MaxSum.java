3d9ea4a7235974e76006270fbcfdf802df9aa4be
ARUP JYOTI DUTTA <anraar1@gmail.com>
2021-03-16 17:03:13 +0530

    Create Mini_MaxSum.java

---
+++ Mini_MaxSum.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the miniMaxSum function below.
+    static void miniMaxSum(int[] arr) {
+
+        Arrays.sort(arr);
+        int len = arr.length;
+        long min,max;
+        min=max=0;
+        
+        for(int i=0;i<4;++i) {
+            min += arr[i];
+            max += arr[len-i-1];
+        }
+        
+        System.out.print(min+" "+max);
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < 5; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        miniMaxSum(arr);
+
+        scanner.close();
+    }
+}
